85c027a67f229d25525dc38f41065f475d057e4e
RealCart <beka-nishanbek@mail.ru>
2025-02-10 16:35:20 +0500

    pre-realase v3.2

--- src/constants/Constance.js
+++ src/constants/Constance.js
@@ -1,4 +1,4 @@
-export const API_KEY = "7c05-109-239-43-25.ngrok-free.app";
+export const API_KEY = "7341-2a03-32c0-300b-47d9-c2db-acd-728b-10.ngrok-free.app";
 export const baseUrl = `https://${API_KEY}/api`;
 export const headers = {
     'ngrok-skip-browser-warning': 'true',
